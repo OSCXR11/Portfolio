8e09d3ec6538b3bbd0871e583148bf477122b03c
OSCXR11 <oscaravendano@yahoo.com>
2025-07-03 23:09:33 -0700

    update pics

--- script.js
+++ script.js
@@ -5,3 +5,19 @@ function toggleMenu() {
   menu.classList.toggle("open");
   icon.classList.toggle("open");
 }
+
+window.addEventListener("DOMContentLoaded", () => {
+  const wrapper = document.getElementById("project1-wrapper");
+  const btn1 = document.querySelector(".hover-btn-1");
+  const btn2 = document.querySelector(".hover-btn-2");
+
+  btn2.addEventListener("mouseenter", () => {
+    wrapper.classList.add("slide-left");
+    wrapper.classList.remove("slide-right");
+  });
+
+  btn1.addEventListener("mouseenter", () => {
+    wrapper.classList.add("slide-right");
+    wrapper.classList.remove("slide-left");
+  });
+});
